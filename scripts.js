67e228f6620dd681963c408efb0705610e21c96f
Konrad Bolesławski <42543331+konradusko@users.noreply.github.com>
2020-06-20 18:40:02 +0200

    init commit
    Init commit already created nav-bar and header section.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("hamburger");
+menu.addEventListener("click", function () {
+
+    if (menu.className === "hamburger active") {
+        menu.classList.remove("active");
+        // menuMobile.classList.remove("Menu-main-mobile-active");
+    } else {
+        menu.className = "hamburger active";
+        // menuMobile.classList.add("Menu-main-mobile-active");
+    }
+})
